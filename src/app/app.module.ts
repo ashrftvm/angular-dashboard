eba72bef4bf5ed05fd265a4a6af57444ef6c5ed3
Ashrf Ali <ashrftvm1@gmail.com>
2019-06-19 16:57:56 +0400

    sidenav menu and material buttons

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { MaterialModule } from './material-module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidenavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
