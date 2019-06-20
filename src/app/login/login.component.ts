c426d9064285ead45da3941778941d30f10ba142
Ashrf Ali <ashrftvm1@gmail.com>
2019-06-20 16:51:54 +0400

    completed facebook login using components

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone} from '@angular/core';
 import { Router } from '@angular/router';
+declare var FB: any;
 
 
 @Component({
@@ -13,15 +14,58 @@ export class LoginComponent implements OnInit {
   password: String;
   error: String;
 
-  constructor(private router : Router) { }
+  constructor(private ngZone: NgZone, private router : Router) { }
 
   ngOnInit() {
+    (window as any).fbAsyncInit = () => {
+      FB.init({appId: '707254099705552',cookie: true, xfbml: true, version: 'v2.8'});
+      FB.Event.subscribe('auth.authResponseChange', (response) => { 
+        if (response.status === 'connected') { 
+          this.statusChangeCallback(response)
+        } else if (response.status === 'not_authorized') { 
+          console.log("Not authorised")
+        } else {
+           console.log("error in verifying")
+          } }); 
+    };
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "https://connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
   }
+
+
+ fetchUserDetail(){
+    FB.api(
+      '/me',
+      'GET',
+      {"fields":"id,name,birthday,email"},
+      (response) => {
+          console.log(response);
+          this.navigate(['/dashboard']);
+      }
+    );
+  }
+
+  public navigate(commands: any[]): void {
+    this.ngZone.run(() => this.router.navigate(commands)).then();
+  } 
+
+  statusChangeCallback(res){
+    if (res.status === 'connected') {
+      this.fetchUserDetail();
+    }
+   }
+  
 
   submit(){
     if(this.username === 'admin' && this.password === 'admin'){
       // console.log(this.username, this.password)
-      this.router.navigate(['/dashboard']);
+      this.router.navigate(['dashboard']);
     }
     else{
       this.error = "Wrong Credentials";
@@ -35,5 +79,10 @@ export class LoginComponent implements OnInit {
   signUp(){
     alert("Yaay! You clicked on Sign Up.");
   }
+
+  fbLoggedIn(){
+    console.log("hI");
+  }
+
 
 }
